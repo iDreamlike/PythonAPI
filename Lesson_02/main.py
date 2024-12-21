15e4af2af32f77d9ad77bee91ff1ea9dd760436c
Dreamlike <bersenev@mail.ru>
2024-12-21 13:37:11 +0300

    Lesson_02 закончил

--- Lesson_02/main.py
+++ Lesson_02/main.py
@@ -53,8 +53,33 @@ import requests
 
 
 # === 5 =============================================================================================
-headers = {"some_header": "123"}
-response = requests.get("https://playground.learnqa.ru/api/show_all_headers", headers = headers)
+# headers = {"some_header": "123"}
+# response = requests.get("https://playground.learnqa.ru/api/show_all_headers", headers = headers)
+#
+# print(response.text)
+# print(response.headers)
 
-print(response.text)
-print(response.headers)
+
+# === 6 =============================================================================================
+# payload = {"login": "secret_login", "password": "secret_pass"}
+# response = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
+#
+# print(response.text)
+# print(response.status_code)
+# print(dict(response.cookies))
+# print(response.headers)
+
+
+# === 7 =============================================================================================
+payload = {"login": "secret_login", "password": "secret_pass"}
+
+response1 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
+cookie_value = response1.cookies.get('auth_cookie')
+cookies = {}
+if cookie_value is not None:
+    cookies.update({'auth_cookie': cookie_value})
+
+
+response2 = requests.post("https://playground.learnqa.ru/api/check_auth_cookie", cookies = cookies)
+
+print(response2.text)
